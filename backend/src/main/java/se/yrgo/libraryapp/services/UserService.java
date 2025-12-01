9032cae2761010d0c5ee90292c9ebe754dfaefd0
Jessica Olofsson <olofsson.jessica@hotmail.com>
2025-12-01 14:34:21 +0100

    Add package.json

--- backend/src/main/java/se/yrgo/libraryapp/services/UserService.java
+++ backend/src/main/java/se/yrgo/libraryapp/services/UserService.java
@@ -3,15 +3,18 @@ package se.yrgo.libraryapp.services;
 import java.util.Optional;
 import javax.inject.Inject;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import se.yrgo.libraryapp.dao.UserDao;
 import se.yrgo.libraryapp.entities.*;
 
 public class UserService {
     private UserDao userDao;
+    private PasswordEncoder encoder;
 
     @Inject
-    UserService(UserDao userDao) {
+    public UserService(UserDao userDao, PasswordEncoder encoder) {
         this.userDao = userDao;
+        this.encoder = encoder;
     }
 
     public Optional<UserId> validate(String username, String password) {
@@ -22,11 +25,21 @@ public class UserService {
 
         LoginInfo loginInfo = maybeLoginInfo.get();
 
-        Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
         if (!encoder.matches(password, loginInfo.getPasswordHash())) {
             return Optional.empty();
         }
 
         return Optional.of(loginInfo.getUserId());
-    }        
+    }
+
+    public boolean validateRegister(String name, String realname, String password) {
+
+        String hashed = encoder.encode(password);
+
+        String sanitizedRealname = realname.replace("'", "\\'");
+
+        return userDao.register(name, sanitizedRealname, hashed);
+    }
+
+
 }
