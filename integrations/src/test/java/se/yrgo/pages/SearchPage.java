de394d690f1cfb37cfbc73056976340ed3d77c5a
Jessica Olofsson <olofsson.jessica@hotmail.com>
2026-01-04 23:02:13 +0100

    Finishing Exercise 2

--- integrations/src/test/java/se/yrgo/pages/SearchPage.java
+++ integrations/src/test/java/se/yrgo/pages/SearchPage.java
@@ -3,16 +3,41 @@ package se.yrgo.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class SearchPage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public SearchPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public WebElement findForm() {
         WebElement formElement = driver.findElement(By.tagName("form"));
         return formElement;
+    }
+
+    public void searchForBookWithTitle(String title) {
+        WebElement input = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[placeholder='Title']")));
+        input.clear();
+        input.sendKeys(title);
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='submit']"))).click();
+    }
+
+    public WebElement bookNotFound() {
+        WebElement notfound = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='No books found']")));
+        return notfound;
+    }
+
+    public WebElement bookFound() {
+        WebElement found = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("caption")));
+        return found;
     }
 }
--- integrations/src/test/resources/se/yrgo/integrations/search.feature
+++ integrations/src/test/resources/se/yrgo/integrations/search.feature
@@ -5,3 +5,13 @@ Scenario: Getting to the search page
 Given the user is on the start page.
 When the user navigates to the book search.
 Then they can see the search form.
+
+Scenario: User searches for a book that does not exist
+  Given the user is on the start page.
+  When the user searches for "Gild"
+  Then "No books found" should be displayed
+
+  Scenario: User searches for a book that exists
+  Given the user is on the start page.
+  When the user searches for "Här kommer Pippi Långstrump"
+  Then "Matching books" should be displayed
