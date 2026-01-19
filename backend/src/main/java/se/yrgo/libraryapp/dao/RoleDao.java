923bccf3a4e590b9b1f3853c880515a9949d121c
Jessica <olofsson.jessica@hotmail.com>
2026-01-19 10:09:04 +0100

    update to PreparedStatement in RoleDao

--- backend/src/main/java/se/yrgo/libraryapp/dao/RoleDao.java
+++ backend/src/main/java/se/yrgo/libraryapp/dao/RoleDao.java
@@ -1,9 +1,6 @@
 package se.yrgo.libraryapp.dao;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -24,18 +21,18 @@ public class RoleDao {
 
     public List<Role> get(UserId userId) {
         List<Role> roles = new ArrayList<>();
-        try (Connection conn = ds.getConnection();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(
-                        "SELECT r.role FROM user_role AS ur JOIN role AS r ON ur.role_id = r.id WHERE ur.user_id = '"
-                                + userId + "'")) {
-            while (rs.next()) {
-                roles.add(Role.fromString(rs.getString("r.role")));
-            }
+        String sql = "SELECT r.role FROM user_role AS ur JOIN role AS r ON ur.role_id = r.id WHERE ur.user_id = ?";
 
+        try (Connection conn = ds.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, userId.getId());
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    roles.add(Role.fromString(rs.getString("r.role")));
+                }
+            }
             return roles;
-        }
-        catch (SQLException ex) {
+        } catch (SQLException ex) {
             logger.error("Unable to get user id", ex);
             return List.of();
         }
